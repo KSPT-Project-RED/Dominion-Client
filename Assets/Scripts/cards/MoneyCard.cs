efc3aab9c877b75a53b2040af662b5b1afad1330
Kislichenko Bogdan <bogdan600@yandex.ru>
2019-12-04 12:32:32 +0300

    add money and win

---
+++ Assets/Scripts/cards/MoneyCard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoneyCard : MonoBehaviour, IPointerClickHandler
+{
+    public GameController gameController;
+    public string name;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Mouse Clicked");
+        gameController.checkBuy(name);
+    }
+
+}
